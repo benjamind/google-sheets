baf971eadc6dc8af4887bf93606a406eba2bd5c3
benjamind <ben@delarre.net>
2013-04-02 21:47:01 -0700

    Added worksheet basics

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var request = require('request'),
 	GoogleClientLogin = require('googleclientlogin').GoogleClientLogin,
 	url = require('url'),
+	builder = require('xmlbuilder'),
 	xml2js = require('xml2js');
 
 function authorize(options, callback) {
@@ -38,6 +39,110 @@ function authorize(options, callback) {
 	googleAuth.login(captcha);
 }
 
+function makeUrl(feed, key) {
+	var url = '';
+	if (feed==='spreadsheets') {
+		url = 'https://spreadsheets.google.com/feeds/spreadsheets/private/full/' + key;
+	} else if (feed === 'worksheets') {
+		url = 'https://spreadsheets.google.com/feeds/worksheets/' + key + '/private/full';
+	}
+	return url;
+}
+
+function Spreadsheet(meta, authId) {
+	this.meta = meta;
+	this.authId = authId;
+}
+Spreadsheet.prototype = {
+
+	query: function(options, callback) {
+		request({
+			url: options.url,
+			method: options.method || 'GET',
+			body: options.body || null,
+			headers: {
+				'Authorization': 'GoogleLogin auth=' + this.authId,
+				'Content-Type': 'application/atom+xml'
+			}
+		}, function(error, response, body) {
+			if (error) {
+				return callback(error);
+			}
+			var parser = new xml2js.Parser({
+				explicitArray: false,
+				async: true,
+				mergeAttrs: true
+			});
+			parser.parseString(body, function(err, result) {
+				callback(err, result);
+			});
+		});
+	},
+
+	parseWorksheet: function(entry) {
+		var worksheet = {
+			lastModified: entry.updated,
+			url: entry.id,
+			title: entry.title._,
+			rowCount: entry['gs:rowCount']._,
+			colCount: entry['gs:colCount']._
+		};
+		return worksheet;
+	},
+
+	getWorksheets: function(callback) {
+		var that = this;
+		this.query({
+			url: makeUrl('worksheets',this.meta.id)
+		}, function(err, result) {
+			if (err) {
+				return callback(err);
+			}
+			var worksheets = [];
+			for(var i=0; i < result.feed.entry.length; i++) {
+				var entry = result.feed.entry[i],
+					worksheet = that.parseWorksheet(entry);
+
+				worksheets.push(worksheet);
+			}
+			callback(null, worksheets);
+		});
+	},
+	addWorksheet: function(data, callback) {
+		var that = this,
+			doc = builder.create(),
+			options = {
+				title: data.title || 'Untitled Worksheet',
+				rowCount: data.rowCount || 50,
+				colCount: data.colCount || 10
+			};
+
+		doc.begin('entry')
+				.att('xmlns','http://www.w3.org/2005/Atom')
+				.att('xmlns:gs','http://schemas.google.com/spreadsheets/2006')
+			.ele('title')
+				.txt(options.title)
+				.up()
+			.ele('gs:rowCount')
+				.txt(options.rowCount)
+				.up()
+			.ele('gs:colCount')
+				.txt(options.colCount);
+
+		this.query({
+			url: makeUrl('worksheets',this.meta.id),
+			method: 'POST',
+			body: doc.toString()
+		}, function(err, data) {
+			if (err) {
+				return callback(err);
+			}
+			var worksheet = that.parseWorksheet(data.entry);
+			callback(null, worksheet);
+		});
+	}
+};
+
 module.exports = {
 	authId: null,
 	version: 0.1,
@@ -51,58 +156,92 @@ module.exports = {
 			callback(null, this.authId);
 		}.bind(this));
 	},
+	parseSheet: function(entry) {
+		var sheet = {
+			lastModified: entry.updated,
+			url: entry.id,
+			author: {
+				name: entry.author.name,
+				email: entry.author.email
+			},
+			title: entry.title._
+		};
+		if (entry.link && entry.link[1] && entry.link[1].href && entry.link[1].rel=='alternate') {
+			var keyUrl = entry.link[1].href,
+				parsedUrl = url.parse(keyUrl,true);
+
+			sheet.key = parsedUrl.query.key;
+		}
+		sheet.id = entry.id.substring(entry.id.lastIndexOf('/')+1);
+		return new Spreadsheet(sheet, this.authId);
+	},
 
 	list: function(callback) {
-		var listUrl = 'https://spreadsheets.google.com/feeds/spreadsheets/private/full';
+		var listUrl = 'https://spreadsheets.google.com/feeds/spreadsheets/private/full', that = this;
 
 		// do request for list of spreadsheets
 		if (!this.authId) {
 			return callback('auth required');
 		}
 
+		this.query({
+			url: listUrl
+		}, function(err, result) {
+			if (err) {
+				return callback(err);
+			}
+			var sheets = [];
+			//console.log(JSON.stringify(result));
+			for(var i=0; i < result.feed.entry.length; i++) {
+				var entry = result.feed.entry[i],
+					sheet = that.parseSheet(entry);
+				sheets.push(sheet);
+			}
+			callback(null,sheets);
+		});
+	},
+
+	query: function(options, callback) {
 		request({
-			url: listUrl,
-			method: 'GET',
+			url: options.url,
+			method: options.method || 'GET',
+			body: options.body || null,
 			headers: {
 				'Authorization': 'GoogleLogin auth=' + this.authId
 			}
-		},function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				// success!
-				var parser = new xml2js.Parser({
-					explicitArray: false,
-					async: true,
-					mergeAttrs: true
-				});
-				parser.parseString(body, function(err, result) {
-					var sheets = [];
-					//console.log(JSON.stringify(result));
-					for(var i=0; i < result.feed.entry.length; i++) {
-						var entry = result.feed.entry[i],
-							sheet = {
-								lastModified: entry.updated,
-								url: entry.id,
-								author: {
-									name: entry.author.name,
-									email: entry.author.email
-								},
-								title: entry.title._
-							};
-						if (entry.link && entry.link[1] && entry.link[1].href && entry.link[1].rel=='alternate') {
-							var keyUrl = entry.link[1].href,
-								parsedUrl = url.parse(keyUrl,true);
-
-							sheet.key = parsedUrl.query.key;
-						}
-						sheet.id = entry.id.substring(entry.id.lastIndexOf('/')+1);
-
-						sheets.push(sheet);
-					}
-					callback(null,sheets);
-				});
-			} else {
-				callback(error);
+		}, function(error, response, body) {
+			if (error) {
+				return callback(error);
 			}
+			var parser = new xml2js.Parser({
+				explicitArray: false,
+				async: true,
+				mergeAttrs: true
+			});
+			parser.parseString(body, function(err, result) {
+				callback(err, result);
+			});
+		});
+	},
+
+	getSpreadsheet: function(key, callback) {
+		// creates a spreadsheet object, having first confirmed the sheet exists
+		//try and get the url
+		if (!this.authId) {
+			return callback('auth required');
+		}
+		var sheetUrl = makeUrl('spreadsheets',key), that = this;
+		this.query({
+			url: sheetUrl
+		},function(err, result) {
+			if (err) {
+				return callback(err);
+			}
+			if (!result) {
+				return callback(null,null);
+			}
+			// assume it worked, parse the spreadsheet
+			callback(null, that.parseSheet(result.entry));
 		});
 	}
 };
--- tests/list.js
+++ tests/list.js
@@ -4,8 +4,31 @@ gsheets.auth({
 	email: process.env.GSHEETS_USER,
 	password: process.env.GSHEETS_PASS
 }, function(err) {
-	gsheets.list(function(err, sheets) {
+
+	/*gsheets.list(function(err, sheets) {
 		console.log('List callback err:' + err);
-		console.log(JSON.stringify(sheets));
+		console.log('Found ' + sheets.length + ' sheets');
+	});*/
+
+	gsheets.getSpreadsheet('0Ak3gStO7i2cYdGhTNlVqaEtuVnJHLTBlaFh2N2k5TVE', function(err, sheet) {
+		console.log('getSpreadsheet callback err:' + err);
+		//console.log('Retrieved sheet :' + JSON.stringify(sheet));
+
+		// get worksheets
+		sheet.getWorksheets(function(err, worksheets) {
+			if (err) {
+				return console.log('ERR: ' + err);
+			}
+			console.log('Found ' + worksheets.length + ' worksheets');
+			//console.log('Worksheet : ' + JSON.stringify(worksheets));
+		});
+		sheet.addWorksheet({
+			title: 'New worksheet'
+		}, function(err, sheet) {
+			if (err) {
+				return console.log('ERR: ' + err);
+			}
+			console.log('Created worksheet : ' + JSON.stringify(sheet));
+		});
 	});
 });
